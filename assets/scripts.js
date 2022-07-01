8e609efdecd5f90460779364e3f6359b05b8c79e
Henry Nguyen <hln11244@gmail.com>
2022-06-30 20:20:52 -0700

    "added save icon"

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,17 @@
 var schedule = $('#schedule')
-
+var sche = document.querySelector('#schedule')
 var currentTime = moment()
+
+// var Saved = localStorage.getItem.parse('saved')
 console.log(currentTime.format('MMM'))
 
 for (var i = 0; i < 24; i++) {
-    var hourBox = $('<div>').addClass('col-12');
-    hourBox.text(i + ":00").addClass('col-3');
-    var inputBox = $('<input>').addClass("col-9").attr('type', 'text')
-    var span = $('<span>').addClass("col-sm-10")
-    schedule.append(hourBox.append(span.append(inputBox)));
+    var hourBox = $('<div>').addClass('col-12').attr('style', 'display:flex');
+    var timeSpan = $('<span>').addClass('col-1');
+    var saveIcon = $('<i>').addClass('col-3 fas fa-save d-flex justify-content-center');
+    timeSpan.text(i + ":00");
+    var inputBox = $('<input>').addClass("col-8").attr('type', 'text').attr('id', "Box" + i + currentTime.format('MMM do YY'))
+    schedule.append(hourBox.append(timeSpan).append(inputBox).append(saveIcon));
 }
+
+schedule.children().children().children().attr('value', 'test')
